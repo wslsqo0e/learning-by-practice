ec9082d6670efba3c24e2c26e44789d1e0f5cd01
shenming <shenming@baidu.com>
2019-11-29 00:46:17 +0800

    add

---
+++ xgboost-learning/cross_validation.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+#####################################################################
+# File Name:  cross_validation.py
+# Author: shenming
+# Created Time: Fri Nov 22 11:05:33 2019
+#####################################################################
+
+import os
+import sys
+
+import numpy as np
+import xgboost as xgb
+
+### load data in do training
+dtrain = xgb.DMatrix('./data/agaricus.txt.train')
+param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic'}
+num_round = 3
+
+print('running cross validation')
+# do cross validation, this will print result out as
+# [iteration]  metric_name:mean_value+std_value
+# std_value is standard deviation of the metric
+res = xgb.cv(param, dtrain, num_round, nfold=5,
+             metrics={'error'}, seed=0,
+             callbacks=[xgb.callback.print_evaluation(show_stdv=True)]
+)
+
+print('running cross validation, disable standard deviation display')
+print(res)
+
+mm = xgb.train(param, dtrain, num_round)
+preds = mm.predict(dtrain)
+labels = dtrain.get_label()
+print('error=%f' % (sum(1 for i in range(len(preds)) if int(preds[i] > 0.5) != labels[i]) / float(len(preds))))
+
+sys.exit(0)
+# do cross validation, this will print result out as
+# [iteration]  metric_name:mean_value
+res = xgb.cv(param, dtrain, num_boost_round=10, nfold=5,
+             metrics={'error'}, seed=0,
+             callbacks=[xgb.callback.print_evaluation(show_stdv=False),
+                        xgb.callback.early_stop(3)])
+
+print(res)
+print('running cross validation, with preprocessing function')
+# define the preprocessing function
+# used to return the preprocessed training, test data, and parameter
+# we can use this to do weight rescale, etc.
+# as a example, we try to set scale_pos_weight
+def fpreproc(dtrain, dtest, param):
+    label = dtrain.get_label()
+    ratio = float(np.sum(label == 0)) / np.sum(label == 1)
+    param['scale_pos_weight'] = ratio
+    return (dtrain, dtest, param)
+
+# do cross validation, for each fold
+# the dtrain, dtest, param will be passed into fpreproc
+# then the return value of fpreproc will be used to generate
+# results of that fold
+xgb.cv(param, dtrain, num_round, nfold=5,
+       metrics={'auc'}, seed=0, fpreproc=fpreproc,
+       callbacks=[xgb.callback.print_evaluation(show_stdv=True)])
+
+###
+# you can also do cross validation with customized loss function
+# See custom_objective.py
+##
+print('running cross validation, with cutomsized loss function')
+
+def logregobj(preds, dtrain):
+    labels = dtrain.get_label()
+    preds = 1.0 / (1.0 + np.exp(-preds))
+    grad = preds - labels
+    hess = preds * (1.0 - preds)
+    return grad, hess
+def evalerror(preds, dtrain):
+    labels = dtrain.get_label()
+    return 'error', float(sum(labels != (preds > 0.0))) / len(labels)
+
+param = {'max_depth':2, 'eta':1, 'silent':1}
+# train with customized objective
+xgb.cv(param, dtrain, num_round, nfold=5, seed=0,
+       obj=logregobj, feval=evalerror,
+       callbacks=[xgb.callback.print_evaluation(show_stdv=True)])
+
+
+
+if __name__ == "__main__":
+    pass
