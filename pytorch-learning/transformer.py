b4b52db9b93d1df041a56f44605045beb4091fde
wslsqo0e <shenming23@126.com>
2019-12-29 00:06:22 +0800

    update transformer

--- pytorch-learning/transformer.py
+++ pytorch-learning/transformer.py
@@ -36,3 +36,57 @@ class EncoderDecoder(nn.Module):
 def clones(module, N):
     "Produce N identical layers"
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+class LayerNorm(nn.Model):
+    "Construct a layernorm module (see citation for details)"
+    def __init__(self, features, eps = 1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim = True)
+        std = x.std(-1, keepdim = True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+class SublayerConnection(nn.Module):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity the norm is first as opposed to last.
+    """
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        "Apply residual connection to any sublayer with the same size."
+        return x = self.dropout(sublayer(self.norm(x)))
+
+class Encoder(nn.Model):
+    "Core encoder is a stack of N layers"
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        "Pass the input (and mask) through each layer in turn"
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+class EncoderLayer(nn.Module):
+    "Encoder is made up of self-attention and feed forward (defined below)"
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x, mask):
+        "Follow Figure 1 (left) for connections."
+        s = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
