cffffa503483a2cc9b376abfdea5db30b7686a99
wslsqo0e <shenming23@126.com>
2019-12-28 23:37:56 +0800

    update pytorch-learning

---
+++ pytorch-learning/transformer.py
@@ -0,0 +1,38 @@
+'''
+reference https://zhuanlan.zhihu.com/p/48731949
+'''
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math, copy, time
+from torch.autograd import Variable
+import matplotlib as plt
+import seaborn
+seaborn.set_context(context = "talk")
+
+class EncoderDecoder(nn.Module):
+    """
+    A standard Encoder-Decoder architecture. Base for this and many other models.
+    """
+    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
+        super(EncoderDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.generator = generator
+
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        "Take in and process masked src and target sequences."
+        return self.decode(self.encode(src, src_mask), src_mas, tgt, tgt_mask);
+
+    def encode(self, src, src_mask):
+        return self.encoder(self.src_embed(src), src_mask)
+
+    def decode(self, memory, src_mask, tgt, tgt_mask):
+        return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
+
+def clones(module, N):
+    "Produce N identical layers"
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
